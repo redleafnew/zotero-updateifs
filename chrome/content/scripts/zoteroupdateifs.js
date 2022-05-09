88496b8e10674fdede2945d83d6498da743b762a
redleafnew <redleafnew@163.com>
2022-05-09 17:24:34 +0800

    增加文献题目中冒号后面为大写字母支持

--- chrome/content/scripts/zoteroupdateifs.js
+++ chrome/content/scripts/zoteroupdateifs.js
@@ -281,6 +281,10 @@ Zotero.UpdateIFs.changeTitleCase = async function() {
                                 replace('england', 'England'). // 替换england
                                 replace('india', 'India'); // 替换india
 
+            //20220509 增加冒号后面为大写字母
+            colon_letter = new_title.match((/(：|:\s*\w)/))[0];  //匹配冒号后面的空格及一个字母
+            new_title = new_title.replace(colon_letter, colon_letter.toUpperCase()); //转为大写
+
            // result += "-> " + new_title + "\n\n";
             // // Do it at your own risk
             //pronew =  title + "\n" + "-> " + new_title + "\n\n" ; //// 20220310
